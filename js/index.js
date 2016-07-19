7d0df5a4da56e371c6bbb7ee00fb270a1af8149d
Alexis Nera <alexisdnera@gmail.com>
2016-07-19 15:25:02 -0400

    Website HTML/CSS/JS edits
    Added portfolio pages. Added media queries. Contact form submission
    fixed.

--- js/index.js
+++ js/index.js
@@ -1,54 +1,51 @@
+// Hamburger menu function
+function toggleMenu(a) {
 
-var scrollLevel = 0;
+	a.preventDefault();
+	
+	if (jQuery(this).hasClass('active')) {
 
+		jQuery(this).removeClass('active');
+		jQuery('.mobileMenu').slideUp();
+		console.log('slide up');
+
+	} else {
+
+		jQuery(this).addClass('active');
+		jQuery('.mobileMenu').slideDown();
+		jQuery(this).hasClass('active');
+		console.log('slide down');
+	}
+}
 
 jQuery(document).ready(function(){
 
-
 	// Prevent right click on images in entire website
 	jQuery('img').bind('contextmenu', function(e) {
-    	return false;
+		return false;
 	});
 
 	// Prevent dragging of all images in entire website
 	jQuery('img').on('dragstart', function(event) { event.preventDefault(); });
 
 
-	// SLICK - HERO image carousel
+	// SLICK - HERO image carousel - autoplay
 	jQuery('.heroCarousel').slick({
 		fade: true,
 		autoplay: true,
 		infinite: true,
-		autoplaySpeed: 10000,
+		autoplaySpeed: 8000,
 		adaptiveHeight: true,
 		cssEase: 'linear',
 		appendArrows: '',
 	});
 
-
-	// NAV SWITCH
-	jQuery("#mainNav").hasClass('hide');
-	// Initial nav hide and main nav show upon scrolling
-	jQuery(window).scroll(function(){
-	   var scroller = jQuery(this).scrollTop();
-	   if (scroller > 300){
-	   		jQuery("#initialNav").fadeOut('slow');
-	   } else {
-	   		jQuery("#initialNav").fadeIn();
-	   }
-	   scrollLevel = scroller;
-	});
-	// Main nav to disappear when user scrolls to 100px from the top
-	jQuery(window).scroll(function(){
-		var scroller = jQuery(this).scrollTop();
-	    if (scroller < 300){
-	    	jQuery("#mainNav").fadeOut('slow').addClass('hide');
-	    } else {
-	    	jQuery("#mainNav").fadeIn('slow').removeClass('hide');
-	    }
-	});
-
+	// Hamburger Menu
+	jQuery('.mobileMenu').hide();
+	jQuery('#menuTrigger').click(toggleMenu);
 
 })
+
+
 
 
